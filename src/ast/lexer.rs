def4b1948d6cd6a050c66b7fa80b441eb92e54f7
Matheus Alves <mathalves@proton.me>
2023-08-21 19:46:18 -0300

    eof rework

--- src/ast/lexer.rs
+++ src/ast/lexer.rs
@@ -5,7 +5,7 @@ pub struct Lexer<'a> {
     pub symbol_table: Rc<RefCell<HashMap<&'a str, TokenKind>>>,
     pub input: Vec<char>,
     pub input_pos: usize,
-    pub ch: char,
+    pub ch: Option<char>,
     pub lex: String,
     pub state: u8,
     pub token: Option<Token>,
@@ -17,7 +17,7 @@ impl<'a> Lexer<'a> {
             symbol_table,
             input,
             input_pos: 0,
-            ch: '#',
+            ch: None,
             lex: String::with_capacity(32),
             state: 0,
             token: None,
@@ -25,23 +25,37 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn is_letter(&self) -> bool {
-        (self.ch >= 'a' && self.ch <= 'z') || (self.ch >= 'A' && self.ch <= 'Z')
+        match self.ch {
+            Some(ch) => {
+                return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
+            }
+            None => {
+                return false;
+            }
+        }
     }
 
     pub fn is_digit(&self) -> bool {
-        self.ch >= '0' && self.ch <= '9'
+        match self.ch {
+            Some(ch) => {
+                return ch >= '0' && ch <= '9';
+            }
+            None => {
+                return false;
+            }
+        }
     }
 
     fn next_char(&mut self) {
         if self.input_pos < self.input.len() {
-            self.ch = self.input[self.input_pos];
+            self.ch = Some(self.input[self.input_pos]);
             self.input_pos += 1;
 
-            if self.ch == '\r' {
+            if self.ch == Some('\r') {
                 self.next_char(); // CRLF
             }
         } else {
-            self.ch = '#'; // EOF
+            self.ch = None; // EOF
             self.input_pos += 1;
         }
     }
@@ -77,51 +91,56 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn state_0(&mut self) {
-        if self.ch == ' ' {
+        if self.ch.is_none() {
+            self.state = 99;
             return;
         }
 
-        self.lex.push(self.ch);
+        let ch = self.ch.unwrap();
 
-        if self.is_letter() || self.ch == '_' {
+        if ch == ' ' {
+            return;
+        }
+
+        self.lex.push(ch);
+
+        if self.is_letter() || ch == '_' {
             self.state = 1;
         } else if self.is_digit() {
             self.state = 2;
-        } else if self.ch == '.' {
+        } else if ch == '.' {
             self.state = 3;
-        } else if self.ch == '=' || self.ch == '<' || self.ch == '>' || self.ch == '!' {
+        } else if ch == '=' || ch == '<' || ch == '>' || ch == '!' {
             self.state = 5;
-        } else if self.ch == '&' || self.ch == '|' {
+        } else if ch == '&' || ch == '|' {
             self.state = 6;
-        } else if self.ch == '/' {
+        } else if ch == '/' {
             self.state = 7;
-        } else if self.ch == '\'' {
+        } else if ch == '\'' {
             self.state = 10;
-        } else if self.ch == '\"' {
+        } else if ch == '\"' {
             self.state = 12;
-        } else if self.ch == '+'
-            || self.ch == '-'
-            || self.ch == '*'
-            || self.ch == '&'
-            || self.ch == '('
-            || self.ch == ')'
-            || self.ch == '{'
-            || self.ch == '}'
-            || self.ch == '['
-            || self.ch == ']'
-            || self.ch == ';'
+        } else if ch == ';'
+            || ch == '+'
+            || ch == '-'
+            || ch == '*'
+            || ch == '&'
+            || ch == '('
+            || ch == ')'
+            || ch == '{'
+            || ch == '}'
+            || ch == '['
+            || ch == ']'
         {
             self.state = 13;
-        } else if self.ch == '\n' {
+        } else if ch == '\n' {
             self.lex.clear();
-        } else if self.ch == '#' {
-            self.state = 99;
         }
     }
 
     pub fn state_1(&mut self) {
         if self.is_letter() || self.is_digit() {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
         } else {
             let kind = self.symbol_table.borrow();
             let kind = kind.get(self.lex.as_str());
@@ -144,9 +163,9 @@ impl<'a> Lexer<'a> {
 
     pub fn state_2(&mut self) {
         if self.is_digit() {
-            self.lex.push(self.ch);
-        } else if self.ch == '.' {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
+        } else if self.ch.is_some() && self.ch.unwrap() == '.' {
+            self.lex.push(self.ch.unwrap());
             self.state = 3;
         } else {
             self.token = Some(Token {
@@ -160,21 +179,21 @@ impl<'a> Lexer<'a> {
 
     pub fn state_3(&mut self) {
         if self.is_digit() {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
             self.state = 4;
-        } else if self.ch == '#' {
+        } else if self.ch.is_none() {
             // eof error
             panic!();
         } else {
             // invalid lex error
-            // self.lex.push(self.ch);
+            // self.lex.push(self.ch.unwrap());
             panic!();
         }
     }
 
     pub fn state_4(&mut self) {
         if self.is_digit() {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
         } else {
             self.token = Some(Token {
                 lex: Some(self.lex.clone()),
@@ -186,8 +205,8 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn state_5(&mut self) {
-        if self.ch == '=' {
-            self.lex.push(self.ch);
+        if self.ch.is_some() && self.ch.unwrap() == '=' {
+            self.lex.push(self.ch.unwrap());
             self.input_pos += 1;
         }
         self.token = Some(Token {
@@ -204,9 +223,9 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn state_6(&mut self) {
-        self.lex.push(self.ch);
+        self.lex.push(self.ch.unwrap());
 
-        if self.lex.starts_with(self.ch) {
+        if self.ch.is_some() && self.lex.starts_with(self.ch.unwrap()) {
             self.token = Some(Token {
                 lex: None,
                 kind: self
@@ -224,7 +243,7 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn state_7(&mut self) {
-        if self.ch == '*' {
+        if self.ch.is_some() && self.ch.unwrap() == '*' {
             self.lex.clear();
             self.state = 7;
         } else {
@@ -233,49 +252,49 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn state_8(&mut self) {
-        if self.ch == '#' {
+        if self.ch.is_none() {
             // eof error
             panic!();
-        } else if self.ch == '*' {
+        } else if self.ch.unwrap() == '*' {
             self.state = 8;
         }
     }
 
     pub fn state_9(&mut self) {
-        if self.ch == '#' {
+        if self.ch.is_none() {
             // eof error
             panic!();
-        } else if self.ch == '/' {
+        } else if self.ch.unwrap() == '/' {
             self.state = 0;
-        } else if self.ch != '*' {
+        } else if self.ch.unwrap() != '*' {
             self.state = 7;
         }
     }
 
     pub fn state_10(&mut self) {
-        if self.ch == '#' {
+        if self.ch.is_none() {
             // eof error
             panic!();
-        } else if !self.ch.is_ascii() {
+        } else if !self.ch.unwrap().is_ascii() {
             // invalid lex error
             // self.lex.push(self.ch);
             panic!();
         } else {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
             self.state = 11;
         }
     }
 
     pub fn state_11(&mut self) {
-        if self.ch == '#' {
+        if self.ch.is_none() {
             // eof error
             panic!();
-        } else if self.ch != '\'' {
+        } else if self.ch.unwrap() != '\'' {
             // invalid lex error
-            // self.lex.push(self.ch);
+            // self.lex.push(self.ch.unwrap());
             panic!();
         } else {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
             self.token = Some(Token {
                 lex: Some(self.lex.clone()),
                 kind: TokenKind::CharValue,
@@ -285,17 +304,17 @@ impl<'a> Lexer<'a> {
     }
 
     pub fn state_12(&mut self) {
-        if self.ch == '#' {
+        if self.ch.is_none() {
             // eof error
             panic!();
-        } else if !self.ch.is_ascii() {
+        } else if !self.ch.unwrap().is_ascii() {
             // invalid lex error
-            // self.lex.push(self.ch);
+            // self.lex.push(self.ch.unwrap());
             panic!();
         } else {
-            self.lex.push(self.ch);
+            self.lex.push(self.ch.unwrap());
 
-            if self.ch == '\"' {
+            if self.ch.unwrap() == '\"' {
                 self.token = Some(Token {
                     lex: Some(self.lex.clone()),
                     kind: TokenKind::StringValue,
