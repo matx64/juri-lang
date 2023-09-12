1b79e6075b850766b67dc97f3f59753059938871
Matheus Alves <mathalves@proton.me>
2023-09-11 21:11:18 -0300

    declaration start

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -6,35 +6,55 @@ use super::{Token, TokenKind};
 pub struct Parser<'a> {
     pub symbol_table: Rc<RefCell<HashMap<&'a str, TokenKind>>>,
     pub lexer: Lexer<'a>,
-    pub current_token: Option<Token>,
-    pub expected_token: Option<Token>,
+    pub current_token: Token,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(symbol_table: Rc<RefCell<HashMap<&'a str, TokenKind>>>, input: Vec<char>) -> Self {
+        let mut lexer = Lexer::new(input, symbol_table.clone());
+        let first_token = lexer.next_token();
         Self {
             symbol_table: symbol_table.clone(),
-            lexer: Lexer::new(input, symbol_table.clone()),
-            current_token: None,
-            expected_token: None,
+            lexer,
+            current_token: first_token,
         }
     }
 
     pub fn start(&mut self) {
-        while self.lexer.input_pos < self.lexer.input.len() {
-            println!("{:?}", self.lexer.next_token());
+        match &self.current_token.kind {
+            TokenKind::IntegerKeyword
+            | TokenKind::FloatKeyword
+            | TokenKind::CharKeyword
+            | TokenKind::StringKeyword => self.declaration(),
+            _ => self.command(),
         }
-
-        // match token.unwrap().kind {
-        //     TokenKind::IntegerKeyword
-        //     | TokenKind::FloatKeyword
-        //     | TokenKind::CharKeyword
-        //     | TokenKind::StringKeyword => self.declaration(),
-        //     _ => self.command(),
-        // }
     }
 
-    pub fn _declaration(&mut self) {}
+    fn match_token(&mut self, expected_kind: TokenKind) -> bool {
+        self.current_token = self.lexer.next_token();
 
-    pub fn _command(&mut self) {}
+        self.current_token.kind == expected_kind
+    }
+
+    fn declaration(&mut self) {
+        let declaration_type = self.current_token.kind.clone();
+
+        if self.match_token(TokenKind::Identifier) {
+            if self.match_token(TokenKind::Atrib) {
+                let expected_kind = match declaration_type {
+                    TokenKind::IntegerKeyword => TokenKind::IntegerValue,
+                    TokenKind::FloatKeyword => TokenKind::FloatValue,
+                    TokenKind::CharKeyword => TokenKind::CharValue,
+                    TokenKind::StringKeyword => TokenKind::StringValue,
+                    _ => unreachable!(),
+                };
+
+                if self.match_token(expected_kind) {
+                    if self.match_token(TokenKind::Semicolon) {}
+                }
+            }
+        }
+    }
+
+    fn command(&mut self) {}
 }
